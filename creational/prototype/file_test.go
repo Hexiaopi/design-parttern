10c89aa9c90333ba4485ab7bb7b75728d753a4ed
hexiaopi <hexiaopi0619@163.com>
2021-08-08 20:31:22 +0800

    原型模式

---
+++ creational/prototype/file_test.go
@@ -0,0 +1,26 @@
+package prototype
+
+func ExampleFile(){
+	file1:=&file{name:"file1"}
+	cloneFile:=file1.clone()
+	cloneFile.print("example_")
+	// Output:
+	// example_file1_clone
+}
+
+func ExampleFolder(){
+	file1:=&file{name:"file1"}
+	file2:=&file{name:"file2"}
+	file3:=&file{name:"file3"}
+	folder1:=&folder{
+		name:"folder1",
+		children:[]inode{file1,file2,file3},
+	}
+	cloneFolder:=folder1.clone()
+	cloneFolder.print("example_")
+	// Output:
+	// example_folder1_clone
+	// example_example_file1_clone
+	// example_example_file2_clone
+	// example_example_file3_clone
+}
