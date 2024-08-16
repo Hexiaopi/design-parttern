f03a3f21a043a3f178d1ade3970537d51e73de75
HeXiaoPi <hechangjie0619@icloud.com>
2024-08-16 17:31:06 +0800

    fix：装饰器模式叠加功能

--- structural/decorator/decorator_test.go
+++ structural/decorator/decorator_test.go
@@ -3,16 +3,25 @@ package decorator
 func ExampleComponent() {
 	c := ConcreteComponent{}
 	c.execute()
+	//c1装饰c
 	var c1 ConcreteDecortor1
 	c1.Decorator.Decorator(c)
 	c1.execute()
+	//c2装饰c
 	var c2 ConcreteDecortor2
 	c2.Decorator.Decorator(c)
 	c2.execute()
+	//c2装饰c1
+	var c21 ConcreteDecortor2
+	c21.Decorator.Decorator(c1)
+	c21.execute()
 	// Output:
 	// concrete component execute
 	// concrete decortor1 add function
 	// concrete component execute
 	// concrete decortor2 add function
+	// concrete component execute
+	// concrete decortor2 add function
+	// concrete decortor1 add function
 	// concrete component execute
 }
